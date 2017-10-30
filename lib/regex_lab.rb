86919fff1db7a9d7c3495dbb7de2f1412ea80c50
Ryan Manchester <ryanm17@gmail.com>
2017-10-30 21:02:51 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,16 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+  (word =~ /\A[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.grep(/\A[un]\z[ing]/i)
+  binding.pry
 end
 
 def words_five_letters_long(text)
+  text.scan(/\w{5}/i)
 
 end
 
