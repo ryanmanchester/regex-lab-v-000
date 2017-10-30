646a685259dba2864f739e5773a96c399f013a74
Ryan Manchester <ryanm17@gmail.com>
2017-10-30 21:38:54 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,8 +5,8 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\A[un]\z[ing]/i)
-  binding.pry
+  text.scan(/\bun.+ing\b/i)
+  #binding.pry
 end
 
 def words_five_letters_long(text)
