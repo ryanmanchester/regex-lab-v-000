6d713c942f7d68b7036dc5c7341decbb580b7756
Ryan Manchester <ryanm17@gmail.com>
2017-10-30 22:06:03 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,10 +12,10 @@ end
 def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
  #binding.pry
-
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+  (text =~ /\w\./i) ? true : false 
 
 end
 
