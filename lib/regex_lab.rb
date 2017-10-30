ee2122dda255b847eca17a171f657ebdf467445a
Ryan Manchester <ryanm17@gmail.com>
2017-10-30 23:10:43 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -20,6 +20,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  (phone =~ /\d/) ? true : false 
+  (phone =~ /\d{10}.+[^\D]/) ? true : false 
 
 end
